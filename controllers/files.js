0f287363a6f367626f57d3a389bad76b86a5e839
Alexander Maximkin <maximkin.alek@yandex.ru>
2021-03-16 21:36:38 +0300

    add route file update

--- controllers/files.js
+++ controllers/files.js
@@ -2,6 +2,18 @@ const db = require('../config/db');
 const fs = require('fs');
 const path = require('path');
 
+// Функция удаления файла из хранилища
+function deleteFile(res, id, sendText) {
+  const filePath = path.join(__dirname, `../my-uploads/${id}`);
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      throw err;
+    }
+    else {
+      res.status(200).send({ message: sendText });
+    }
+  });
+}
 
 // Добавить файл
 module.exports.addFile = async (req, res) => {
@@ -34,7 +46,7 @@ module.exports.getFilesInfo = async (req, res) => {
 module.exports.deleteFile = async (req, res) => {
 
   const id = req.params.id;
-  await db.query(" SELECT `id`, `name`, `extension`, `mime-type`, `size`, `date` FROM `files` WHERE name = '" + id + "'", (err, result) => {
+  await db.query(" SELECT `id` FROM `files` WHERE name = '" + id + "'", (err, result) => {
     if (err) {
       res.status(400).send({ message: err });
     } else if (result.length <= 0) {
@@ -43,22 +55,13 @@ module.exports.deleteFile = async (req, res) => {
     else {
       db.query(" DELETE FROM `files` WHERE name = '" + id + "'", (err, result) => {
         if (err) {
-          res.status(400).send({ message: err });
+          res.status(500).send({ message: err });
         } else {
-          const filePath = path.join(__dirname, `../my-uploads/${id}`);
-          fs.unlink(filePath, (err) => {
-            if (err) {
-              throw err;
-            }
-            else {
-              res.status(200).send({ message: 'Файл успешно удален' });
-            }
-          });
+          deleteFile(res, id, 'Файл успешно удален');
         }
       })
     }
   })
-
 }
 // Получить информацию о файле
 module.exports.getFileInfo = async (req, res) => {
@@ -72,3 +75,27 @@ module.exports.getFileInfo = async (req, res) => {
     }
   })
 }
+module.exports.updateFile = async (req, res) => {
+  const id = req.params.id;
+  // Проверяем наличие файла
+  await db.query(" SELECT `id` FROM `files` WHERE name = '" + id + "'", (err, result) => {
+    if (err) {
+      res.status(400).send({ message: err });
+    } else if (result.length <= 0) {
+      res.status(404).send({ message: 'Файл не найден' });
+    }
+    else {
+      // Обновляем файл
+      const { filename, originalname, mimetype, size } = req.file;
+      const ext = originalname.substr(originalname.lastIndexOf('.') + 1);
+      db.query("UPDATE `files` SET `name`='" + filename + "',`extension`='" + ext + "',`mime-type`='" + mimetype + "',`size`='" + size + "',`date`= NOW() WHERE name = '" + id + "'", (err, result) => {
+        if (err) {
+          res.status(500).send({ message: err });
+        } else {
+          deleteFile(res, id, 'Файл успешно обновлен');
+        }
+      })
+    }
+  })
+}
+
